408cbf0900d2263bac3423a6f3f4c231a05e1e3b
Aaron Hom <aaron.w.hom@gmail.com>
2020-03-01 20:04:47 -0800

    dates print in right order, friday is no longer a weekend

--- calendar/scheduler.py
+++ calendar/scheduler.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from datetime import date
 from datetime import timedelta
 today = date.today()
@@ -5,12 +7,13 @@ today = date.today()
 def printSchedule(x):
     global today
     for i in range(0, x):
-        if(today.weekday() < 4):
+        if(today.weekday() < 5):
             print(today.strftime("%Y.%m.%d-%a"))
             print("today:\nyesterday:\nblockers:\n")
         else: print(today.strftime("%Y.%m.%d-%a") + "\n")
-        today += timedelta(days = 1)
+        today -= timedelta(days = 1)
                 
 print("Days to display: ")
 numDays = int(input())
+today += timedelta(days = numDays-1)
 printSchedule(numDays)
